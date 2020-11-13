69dd085691c41a8007eb871de93c54950602b007
sonylomo <49971500+sonylomo@users.noreply.github.com>
2020-11-13 11:54:15 +0300

    More responsive

--- src/App.js
+++ src/App.js
@@ -3,12 +3,6 @@ import Checkout from './components/Checkout/Checkout'
 import 'bootstrap/dist/css/bootstrap.css'
 import ImagePreview from './components/ImagePreview/ImagePreview'
 import { Container, Row, Col } from 'react-bootstrap'
-/*const container = (
-  <Container>
-    <ImagePreview price={this.state.price} duration={this.state.duration} people={this.state.people} image="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/jj-2.jpg" />
-    <Checkout duration={this.state.duration} discount={this.state.discount} tax={this.state.tax} price={this.state.price} onSubmit={this.handleSubmit} />
-  </Container>
-);*/
 
 function App() {
   const initialState = {
